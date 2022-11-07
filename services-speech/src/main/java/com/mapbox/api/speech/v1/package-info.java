854e8cab2585f143b772502402a8da68007d24ed
Swagata Prateek <pswagata@amazon.com>
2022-11-07 15:37:24 -0800

    Removed tile-query, and speech

--- services-speech/src/main/java/com/mapbox/api/speech/v1/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains the Mapbox Java Services wrapper for the Speech API.
- *
- * @since 3.0.0
- */
-package com.mapbox.api.speech.v1;
--- services-tilequery/src/main/java/com/mapbox/api/tilequery/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contains the Mapbox Java Services classes related to the Mapbox Tilequery API.
- */
-package com.mapbox.api.tilequery;
